347229d22ae6b442acec0316a0e43fb7f136e576
ngom52 <fezmeister87@gmail.com>
2023-01-11 14:53:59 +0800

    Change filepath

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
-def get_todos(filepath='C:/Users/10091992/Documents/PycharmProjects/pythonProject/todos.txt'):
+def get_todos(filepath='../web_app1/todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath='C:/Users/10091992/Documents/PycharmProjects/pythonProject/todos.txt'):
+def write_todos(todos_arg, filepath='../web_app1/todos.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
