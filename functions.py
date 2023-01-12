8330dd9ebe07f666033f44802ff7aaf92d1543cd
ngom52 <fezmeister87@gmail.com>
2023-01-12 08:47:04 +0800

    Added alternate filepath with forward slashes

--- functions.py
+++ functions.py
@@ -1,9 +1,19 @@
-def get_todos(filepath='../web_app1/todos.txt'):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+try:
+    def get_todos(filepath='../web_app1/todos.txt'):
+        with open(filepath, 'r') as file_local:
+            todos_local = file_local.readlines()
+        return todos_local
+except FileNotFoundError:
+    def get_todos(filepath='..\web_app1\todos.txt'):
+        with open(filepath, 'r') as file_local:
+            todos_local = file_local.readlines()
+        return todos_local
 
-
-def write_todos(todos_arg, filepath='../web_app1/todos.txt'):
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+try:
+    def write_todos(todos_arg, filepath='../web_app1/todos.txt'):
+        with open(filepath, 'w') as file:
+            file.writelines(todos_arg)
+except FileNotFoundError:
+    def write_todos(todos_arg, filepath='..\web_app1\todos.txt'):
+        with open(filepath, 'w') as file:
+            file.writelines(todos_arg)
