81d6250f55e09380c70298cb61bbd63e7ebb64d1
ngom52 <fezmeister87@gmail.com>
2023-01-12 09:30:58 +0800

    Used pathlib module to find the root of the folder on github

--- functions.py
+++ functions.py
@@ -1,19 +1,15 @@
-try:
-    def get_todos(filepath='../web_app1/todos.txt'):
-        with open(filepath, 'r') as file_local:
-            todos_local = file_local.readlines()
-        return todos_local
-except FileNotFoundError:
-    def get_todos(filepath='..\web_app1\todos.txt'):
-        with open(filepath, 'r') as file_local:
-            todos_local = file_local.readlines()
-        return todos_local
+from pathlib import Path
 
-try:
-    def write_todos(todos_arg, filepath='../web_app1/todos.txt'):
-        with open(filepath, 'w') as file:
-            file.writelines(todos_arg)
-except FileNotFoundError:
-    def write_todos(todos_arg, filepath='..\web_app1\todos.txt'):
-        with open(filepath, 'w') as file:
-            file.writelines(todos_arg)
+todos_path = Path(__file__).parents[1]/'web_app1/todos.txt'
+
+
+def get_todos(filepath=todos_path):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+#'../web_app1/todos.txt'
+def write_todos(todos_arg, filepath=todos_path):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- web.py
+++ web.py
@@ -3,10 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
+
 
 st.title('My Todo App')
 st.subheader('This is my Todo App')
